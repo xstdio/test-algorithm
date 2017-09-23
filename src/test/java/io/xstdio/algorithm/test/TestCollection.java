7d2df5a755645f0f03a20764b6db4813ebcbdaff
jason chen <jason.chen@senseinfosys.com>
2017-09-23 17:25:34 +0800

    add collection recursive sort

--- src/test/java/io/xstdio/algorithm/test/TestCollection.java
+++ src/test/java/io/xstdio/algorithm/test/TestCollection.java
@@ -36,88 +36,90 @@ public class TestCollection {
 		System.out.println(linkedlist.size());
 		System.out.println(linkedlist);
 	}
-	
-	@Test
-	public void hash_table() {
-		Hashtable table = new Hashtable();
-        table.put("Sony", "Bravia");
-        table.put("Samsung", "Galaxy");
-        table.put("Nokia", "Lumia");
 
-        System.out.println("does hash table has Lumia as value : " + table.containsValue("Lumia"));
-        System.out.println("does hash table Lumia as key : " + table.containsKey("Lumia"));
-        
-      //finding key corresponding to value in hashtable - one to one mapping
-        String key= null;
-        String value="Lumia";
-        for(Map.Entry entry: table.entrySet()){
-            if(value.equals(entry.getValue())){
-                key = entry.getKey();
-                break; //breaking because its one to one map
-            }
-        }
-        System.out.println("got key from value in hashtable key:  "+ key +" value: " + value);
-
-      //how to get from value in HashMap example - similar to Hashtable example
-        HashMap map = new HashMap();
-        map.put("one", 1);
-        map.put("two", 2);
-      
-        //find key from value in HashMap Java - one to one mapping
-        Integer intValue=2;
-        String strKey = null;
-        for(Map.Entry entry: map.entrySet()){
-            if(intValue.equals(entry.getValue())){
-                strKey = entry.getKey();
-                break; //breaking because its one to one map
-            }
-        }
-      
-        System.out.println("key from value in hash table key:  "+ strKey +" value: " + intValue);
-
-
-
+	// @Test
+	// public void hash_table() {
+	// Hashtable table = new Hashtable();
+	// table.put("Sony", "Bravia");
+	// table.put("Samsung", "Galaxy");
+	// table.put("Nokia", "Lumia");
+	//
+	// System.out.println("does hash table has Lumia as value : " +
+	// table.containsValue("Lumia"));
+	// System.out.println("does hash table Lumia as key : " +
+	// table.containsKey("Lumia"));
+	//
+	// // finding key corresponding to value in hashtable - one to one mapping
+	// String key = null;
+	// String value = "Lumia";
+	// for (Map.Entry entry : table.entrySet()) {
+	// if (value.equals(entry.getValue())) {
+	// key = entry.getKey();
+	// break; // breaking because its one to one map
+	// }
+	// }
+	// System.out.println("got key from value in hashtable key: " + key + "
+	// value: " + value);
+	//
+	// // how to get from value in HashMap example - similar to Hashtable
+	// // example
+	// HashMap map = new HashMap();
+	// map.put("one", 1);
+	// map.put("two", 2);
+	//
+	// // find key from value in HashMap Java - one to one mapping
+	// Integer intValue = 2;
+	// String strKey = null;
+	// for (Map.Entry entry : map.entrySet()) {
+	// if (intValue.equals(entry.getValue())) {
+	// strKey = entry.getKey();
+	// break; // breaking because its one to one map
+	// }
+	// }
+	// System.out.println("key from value in hash table key: " + strKey + "
+	// value: " + intValue);
+	// }
+	/*
+	 * detect duplicate in array by comparing size of List and Set since Set
+	 * doesn't contain duplicate, size must be less for an array which contains
+	 * duplicates
+	 */
+	public static boolean check_duplicate_set_size(String[] input) {
+		List<String> inputList = Arrays.asList(input);
+		Set<String> inputSet = new HashSet<String>(inputList);
+		if (inputSet.size() < inputList.size())
+			return true;
+		return false;
 	}
 
 	/*
-     * detect duplicate in array by comparing size of List and Set
-     * since Set doesn't contain duplicate, size must be less for an array which contains duplicates
-     */
-    public static boolean check_duplicate_set_size(String[] input){
-        List<String> inputList = Arrays.asList(input);
-        Set<String> inputSet = new HashSet<String>(inputList);
-        if(inputSet.size()< inputList.size())
-            return true;
-        return false;
-    }
-    /*
-     * Since Set doesn't allow duplicates add() return false
-     * if we try to add duplicates into Set and this property
-     * can be used to check if array contains duplicates in Java
-     */
-    public static boolean check_duplicate_set_add(String[] input) {
-        Set<String> tempSet = new HashSet<String>();
-        for (String str : input) {
-            if (!tempSet.add(str)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	 * Since Set doesn't allow duplicates add() return false if we try to add
+	 * duplicates into Set and this property can be used to check if array
+	 * contains duplicates in Java
+	 */
+	public static boolean check_duplicate_set_add(String[] input) {
+		Set<String> tempSet = new HashSet<String>();
+		for (String str : input) {
+			if (!tempSet.add(str)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    @Ignore
+	@Ignore
 	@Test
 	public void duplicate_detection() {
-		String[] dup_ar = new String[] {"one","two","three","one"};
-		String[] no_dup_ar = new String[] {"one","two","three"};
-		
+		String[] dup_ar = new String[] { "one", "two", "three", "one" };
+		String[] no_dup_ar = new String[] { "one", "two", "three" };
+
 		System.out.println("dup_check_set_size:" + check_duplicate_set_size(dup_ar));
 		System.out.println("no_dup_check_set_size:" + check_duplicate_set_size(no_dup_ar));
-		
+
 		System.out.println("dup_check_set_add:" + check_duplicate_set_add(dup_ar));
 		System.out.println("no_dup_check_set_add:" + check_duplicate_set_add(no_dup_ar));
 	}
-	
+
 	public static String string_iterative_reverse(String str) {
 		StringBuilder strBuilder = new StringBuilder();
 		char[] strChars = str.toCharArray();
@@ -126,26 +128,27 @@ public class TestCollection {
 		}
 		return strBuilder.toString();
 	}
+
 	public static String string_recusive_reverse(String str) {
 		// base case to handle one char string and empty string
 		if (str.length() < 2) {
 			return str;
 		}
-		System.out.printf("\n |%s|%s|%s|",str.substring(1), str.charAt(0), str);
+		System.out.printf("\n |%s|%s|%s|", str.substring(1), str.charAt(0), str);
 		return string_recusive_reverse(str.substring(1)) + str.charAt(0);
 	}
-	
+
 	@Ignore
 	@Test
 	public void string_reverse() {
 		String test = "this is a test msg";
-		//stringbuffer
+		// stringbuffer
 		StringBuffer buffer = new StringBuffer(test);
 		System.out.printf("|%s|\n|%s|", test, buffer.reverse().toString());
-		//iterative
-        System.out.printf("\nIteration: |%s|", string_iterative_reverse(test));
-        //recursive
-        System.out.printf("\nRecursion: |%s|", string_recusive_reverse(test));
+		// iterative
+		System.out.printf("\nIteration: |%s|", string_iterative_reverse(test));
+		// recursive
+		System.out.printf("\nRecursion: |%s|", string_recusive_reverse(test));
 	}
 
 	/**
@@ -182,15 +185,48 @@ public class TestCollection {
 			}
 			current = current.next();
 		}
-
 		if (length % 2 == 1) {
 			middle = middle.next();
 		}
 
 		System.out.println("length of LinkedList: " + length);
 		System.out.println("middle element of LinkedList : " + middle);
-
 	}
+
+	private boolean isPalindrome(int number) {
+		if (number == reverse(number)) {
+			return true;
+		}
+		return false;
+	}
+
+	private int reverse(int number) {
+		int reverse = 0;
+
+		while (number != 0) {
+			reverse = reverse * 10 + number % 10;
+			number = number / 10;
+			System.out.printf("|reverse:%d,number:%d", reverse, number);
+		}
+		
+		System.out.printf("[%d]",reverse);
+		return reverse;
+	}
+	
+	
+
+	@Test
+	public void palindrome() {
+		int[] numbers = { 1, 20, 22, 102, 101, 1221, 13321, 13331, 0, 11 };
+		for (int number : numbers) {
+//			System.out.printf("\n [%d]",number);
+//			isPalindrome(number);
+//			System.out.println("\nDoes number : " + number + " is a palindrome? " + isPalindrome(number));
+		}
+		
+		
+	}
+
 }
 
 class LinkedList {
--- src/test/java/io/xstdio/algorithm/test/TestRecursive.java
+++ src/test/java/io/xstdio/algorithm/test/TestRecursive.java
@@ -10,45 +10,96 @@ import org.junit.Test;
 public class TestRecursive {
 
 	public int simple_recursive(int aVariable) {
-//		System.out.println(aVariable);
+		// System.out.println(aVariable);
 		aVariable--;
 		if (aVariable == 0)
 			return 0;
 		return simple_recursive(aVariable);
 	}
+
 	public long fibonacci_recursive(int n1, int n2, int idx, int stop) {
-//		System.out.printf("\n%d %d %d %d",n1, n2, idx, stop);
-		if(idx==3) {
-			System.out.printf("1:%d\n2:%d",n1,n2);
+		// System.out.printf("\n%d %d %d %d",n1, n2, idx, stop);
+		if (idx == 3) {
+			System.out.printf("1:%d\n2:%d", n1, n2);
 		}
-		System.out.printf("\n%d:%d",idx, (n1+n2));
-		
-		if(idx==stop) {
-			return n1+n2;
+		System.out.printf("\n%d:%d", idx, (n1 + n2));
+		if (idx == stop) {
+			return n1 + n2;
 		}
 		idx++;
-		return fibonacci_recursive(n2,n1+n2,idx,stop);
+		return fibonacci_recursive(n2, n1 + n2, idx, stop);
 	}
-	public int factorial_recursive(int number){       
-        //base case
-        if(number == 0){
-            return 1;
-        }
-        return number*factorial_recursive(number -1); 
-    }
 
+	public int factorial_recursive(int number) {
+		// base case
+		if (number == 0) {
+			return 1;
+		}
+		return number * factorial_recursive(number - 1);
+	}
+	
+	public long reverse_recursive(long number, long reverse) {
+		if(number == 0)
+			return reverse;
+		reverse = reverse*10 + number%10;
+		
+		return reverse_recursive(number/10, reverse);
+	}
 
 //	@Ignore
 	@Test
 	public void recursive() {
-		
-		//simple
-		System.out.printf("===\nsimple:%s\n===\n",simple_recursive (10));
-		//fibonacci
-		System.out.printf("\n===\nfibonacci:%s",fibonacci_recursive (0,1,3,40));
-		//factorial
-		System.out.printf("\n===\nfactorial:%s", factorial_recursive(5)); 
+		// simple
+		System.out.printf("===\nsimple:%s\n===\n", simple_recursive(10));
+		// fibonacci
+		System.out.printf("\n===\nfibonacci:%s", fibonacci_recursive(0, 1, 3, 40));
+		// factorial
+		System.out.printf("\n===\nfactorial:%s", factorial_recursive(5));
+		// reverse
+		System.out.printf("\n===\nreverse:13321:%d",reverse_recursive(13321, 0));
 	}
-	
-	
+
+	int min(int x, int y, int z) {
+		if (x < y && x < z)
+			return x;
+		if (y < x && y < z)
+			return y;
+		else
+			return z;
+	}
+
+	int editDist(String str1, String str2, int m, int n) {
+		// If first string is empty, the only option is to
+		// insert all characters of second string into first
+		if (m == 0)
+			return n;
+
+		// If second string is empty, the only option is to
+		// remove all characters of first string
+		if (n == 0)
+			return m;
+
+		// If last characters of two strings are same, nothing
+		// much to do. Ignore last characters and get count for
+		// remaining strings.
+		if (str1.charAt(m - 1) == str2.charAt(n - 1))
+			return editDist(str1, str2, m - 1, n - 1);
+
+		// If last characters are not same, consider all three
+		// operations on last character of first string, recursively
+		// compute minimum cost for all three operations and take
+		// minimum of three values.
+		return 1 + min(editDist(str1, str2, m, n - 1), // Insert
+				editDist(str1, str2, m - 1, n), // Remove
+				editDist(str1, str2, m - 1, n - 1) // Replace
+		);
+	}
+	@Ignore
+	@Test
+	public void edit_distance() {
+		String str1 = "sunday";
+		String str2 = "saturday";
+
+		System.out.println(editDist(str1, str2, str1.length(), str2.length()));
+	}
 }
